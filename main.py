0523d6956c167311183bbc381ad526e1ad40a2d4
VihrKraft <sanefedov09@yandex.ru>
2025-04-12 17:32:23 +0300

    Начал писать пульт охраны банка

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
@@ -8,5 +9,7 @@ django.setup()
 from datacenter.models import Passcard  # noqa: E402
 
 if __name__ == '__main__':
-    # Программируем здесь
-    print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
+    passcards = Passcard.objects.all()
+    for passcard in passcards:
+        active_passcards = Passcard.objects.filter(is_active=True)
+print(f"Всего пропусков {Passcard.objects.count()}\nАктивных пропусков {len(active_passcards)}")
--- settings.py
+++ settings.py
@@ -1,17 +1,22 @@
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': '',
-        'PORT': '',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
+        'HOST': os.getenv('HOST'),
+        'PORT': '5434',
+        'NAME': 'checkpoint',
+        'USER': 'guard',
+        'PASSWORD': os.getenv('PASSWORD'),
     }
 }
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = 'REPLACE_ME'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 TIME_ZONE = 'Europe/Moscow'
 
